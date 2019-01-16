f4b8d366d272b4e978dc54f627b0f10e06edfd3f
nobita <1057735161@qq.com>
2019-01-16 14:57:37 +0800

    测试提交

---
+++ app/src/main/java/com/nobitastudio/materialdesign/bean/Test.java
@@ -0,0 +1,12 @@
+package com.nobitastudio.materialdesign.bean;
+
+import java.io.Serializable;
+
+/**
+ * @author chenxiong
+ * @email nobita0522@qq.com
+ * @date 2019/01/16 14:57
+ * @description
+ */
+public class Test implements Serializable {
+}
